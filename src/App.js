379234627a4100c7d78ab46f14291301fa3048ae
Maiken Gunnerod <maiken.gunneroed@gmail.com>
2024-04-10 21:41:47 +0200

    migrate content

--- src/App.js
+++ src/App.js
@@ -4,22 +4,23 @@ import Layout from "./pages/Layout";
 import Main from "./pages/Main";
 import Info from "./pages/Info";
 import NoPage from "./pages/NoPage";
+import "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Main />} />
-          <Route path="Info" element={<Info />} />
-          <Route path="*" element={<NoPage />} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Main />} />
+					<Route path="Info" element={<Info />} />
+					<Route path="*" element={<NoPage />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	);
 }
 
 export default App;
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -1,5 +1,126 @@
-import React from 'react';
+import { React, useState } from "react";
+import ExtraMain from "./ExtraMain.js";
+import Newsletter from ".././components/Newsletter.js";
+// import { collection, addDoc } from "firebase/firestore";
+// import db from "../firebase-config";
 
 export default function Main() {
-  return <h2>main page content here</h2>;
+	return (
+		<div>
+			<main>
+				<section class="section--main-content">
+					<div class="subsection--main-release">
+						<iframe
+							src="https://www.youtube.com/embed/CT5tn7OUqsg?si=kuSqQsbBx7SklBmM"
+							title="YouTube video player"
+							frameborder="0"
+							allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+							allowfullscreen
+						></iframe>
+						{/* <img
+							src="site-content/psalm.png"
+							class="music-cover"
+							alt="psalm. by SAI's Fate. cover art"
+						></img> */}
+						<h1 id="release-title">lullaby.</h1>
+						<p id="creator-title">SAI's fate.</p>
+					</div>
+					<div class="subsection--listen-on">
+						<h2 class="subheading" id="subheading--listen-on">
+							listen on
+						</h2>
+						<div class="logo-button-container">
+							<a
+								href="https://www.youtube.com/watch?v=CT5tn7OUqsg"
+								target="_blank"
+								rel="noreferrer"
+								aria-label="YouTube"
+								class="logo-button"
+							>
+								<i class="fa-brands fa-youtube"></i>
+							</a>
+							<a
+								href="https://open.spotify.com/track/5Qu003Ay3pWHsoEUmt6yoa?si=98a20f27eed4406b"
+								target="_blank"
+								rel="noreferrer"
+								aria-label="Spotify"
+								class="logo-button"
+							>
+								<i class="fa-brands fa-spotify"></i>
+							</a>
+							<a
+								href="https://soundcloud.com/saisfate/lullaby"
+								target="_blank"
+								rel="noreferrer"
+								aria-label="Soundcloud"
+								class="logo-button"
+							>
+								<i class="fa-brands fa-soundcloud"></i>
+							</a>
+						</div>
+						<div class="text-button-container" id="social-buttons">
+							<a
+								href="https://www.instagram.com/omosaisfate/"
+								target="_blank"
+								rel="noreferrer"
+								class="text-button"
+							>
+								<i class="fa-brands fa-instagram"></i>
+								<p>Instagram</p>
+							</a>
+							<a
+								href="https://www.tiktok.com/@saisfate"
+								target="_blank"
+								rel="noreferrer"
+								class="text-button"
+							>
+								<i class="fa-brands fa-tiktok"></i>
+								<p>TikTok</p>
+							</a>
+							<a
+								href="https://www.snapchat.com/add/omo541"
+								target="_blank"
+								rel="noreferrer"
+								class="text-button"
+							>
+								<i class="fa-brands fa-snapchat"></i>
+								<p>Snapchat</p>
+							</a>
+						</div>
+					</div>
+				</section>
+				<ExtraMain />
+				<Newsletter />
+			</main>
+
+			<footer>
+				<a
+					href="https://www.youtube.com/@saisfate"
+					target="_blank"
+					rel="noreferrer"
+					class="footer-logo-wrapper footer-logo-wrapper--omo"
+				>
+					<img
+						src="utilities/branding/omologo.png"
+						alt="OMO logo"
+						class="footer-logo"
+						id="footer-logo--omo"
+					/>
+				</a>
+				{/* <a
+					href="https://www.youtube.com/@omoweatherstation"
+					target="_blank"
+					rel="noreferrer"
+					class="footer-logo-wrapper footer-logo-wrapper--weatherstation"
+				>
+					<img
+						src="utilities/branding/weatherstation_logo.png"
+						alt="OMOweatherstation logo"
+						class="footer-logo"
+						id="footer-logo--weatherstation"
+					/>
+				</a> */}
+			</footer>
+		</div>
+	);
 }
