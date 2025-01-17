cf66c49163aef43b2805b329256c5a5509e1e1cc
L0u155 <louis.matsika@bbc.co.uk>
2025-01-17 15:32:15 +0000

    make title component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import Head from "next/head";
 import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
 import styles from "#/styles/Home.module.css";
+import ArtistBrand from "#/components/ArtistBrand";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -26,7 +27,7 @@ export default function Home() {
         className={`${styles.page} ${geistSans.variable} ${geistMono.variable}`}
       >
         <main className={styles.main}>
-          <h1>SAI's fate.</h1>
+          <ArtistBrand />
         </main>
         <footer className={styles.footer}>
           <a
