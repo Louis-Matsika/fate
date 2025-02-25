bc1249dc2cefc9b30c36bf31c0bf57ecd6d5c1bf
Louis Matsika <louis.matsika@bbc.co.uk>
2025-02-25 19:45:06 +0000

    go back to nextjs app router

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,14 +1,16 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
-import pluginReact from "eslint-plugin-react";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
-  {languageOptions: { globals: globals.browser }},
-  pluginJs.configs.recommended,
-  ...tseslint.configs.recommended,
-  pluginReact.configs.flat.recommended,
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [
+  ...compat.extends("next/core-web-vitals", "next/typescript"),
 ];
+
+export default eslintConfig;
--- src/components/ArtistBrand/index.tsx
+++
@@ -1,5 +0,0 @@
-import React from "react"
-
-const ArtistBrand =()=> {
-    return <h1>SAI</h1>
-}
