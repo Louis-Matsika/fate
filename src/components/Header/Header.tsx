59ab1d4fe31b1cfa07a927aaf71cc75d400b8133
Louis Matsika <louis.matsika@bbc.co.uk>
2025-03-01 13:38:49 +0000

    make initial emotion styled comp

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,13 @@
+'use client'
+/** @jsxRuntime classic */
 /** @jsx jsx */
+
 import { jsx } from '@emotion/react'
 
-import React from 'react';
+import styles from "./index.styles";
 
-const Header =()=>{
-    return(
-    <div css={{
-        backgroundColor: 'hotpink',
-        '&:hover': {
-          color: 'lightgreen'
-        }
-      }}>
-        header
-    </div>);
-}
+const Header = () => {
+  return <div css={styles.div}>header</div>;
+};
 
 export default Header;
--- src/components/Header/index.styles.ts
+++ src/components/Header/index.styles.ts
@@ -1,9 +1,13 @@
-// import { css } from '@emotion/react';
+import { css } from '@emotion/react';
 
-// // eslint-disable-next-line import/no-anonymous-default-export
-// export default {
-//     componentContainer: () =>
-//       css({
-//         justifyContent: 'center',
-//       }),
-//     }
+// eslint-disable-next-line import/no-anonymous-default-export
+export default {
+    div: () =>
+      css({
+        justifyContent: 'center',
+        backgroundColor: 'hotpink',
+        '&:hover': {
+          color: 'lightgreen'
+        }
+      }),
+    }
