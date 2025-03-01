2405e1abe2fc6a79666b998253b3a2834880496d
Louis Matsika <louis.matsika@bbc.co.uk>
2025-03-01 16:11:40 +0000

    set up basic themeProvider

--- src/app/index.styles.ts
+++ src/app/index.styles.ts
@@ -1,8 +1,12 @@
-import { css } from '@emotion/react';
+import { css } from "@emotion/react";
+import withThemeProvider from "#/themes/withThemeProvider";
+
+// Define a type for the colours parameter
+type ColoursType = typeof withThemeProvider.colours;
 
 export default {
-    div: () =>
-      css({
-        backgroundColor: 'teal',
-      }),
-    }
+  div: ({ colours = withThemeProvider.colours }: { colours?: ColoursType }) =>
+    css({
+      backgroundColor: colours.BLACK,
+    }),
+};
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,24 @@
-'use client'
+"use client";
 /** @jsxImportSource @emotion/react */
-
-import { ThemeProvider } from '@emotion/react'
 
 import Header from "#/components/Header/Header";
 import Frame from "#/components/Frame/Frame";
 import Footer from "#/components/Footer/Footer";
 
+import { ThemeProvider } from "@emotion/react";
+import withThemeProvider from "#/themes/withThemeProvider";
 import styles from "./index.styles";
 
-const theme = {
-  colors: {
-    primary: 'hotpink'
-  }
-}
-
-const Home=()=> {
+const Home = () => {
   return (
-    <ThemeProvider theme={theme}>
-    <div css={styles.div}>
-      <Header />
-      <Frame />
-      <Footer />
-    </div>
+    <ThemeProvider theme={withThemeProvider}>
+      <div css={styles.div}>
+        <Header />
+        <Frame />
+        <Footer />
+      </div>
     </ThemeProvider>
   );
-}
+};
 
 export default Home;
