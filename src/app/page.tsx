c7da44a951c41d5becf1e05be37b96e73204ce6b
Louis Matsika <louis.matsika@bbc.co.uk>
2025-03-01 15:15:01 +0000

    add basic themeProvider

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,29 @@
 'use client'
 /** @jsxImportSource @emotion/react */
 
-import styles from "./index.styles";
+import { ThemeProvider } from '@emotion/react'
 
 import Header from "#/components/Header/Header";
 import Frame from "#/components/Frame/Frame";
 import Footer from "#/components/Footer/Footer";
 
+import styles from "./index.styles";
+
+const theme = {
+  colors: {
+    primary: 'hotpink'
+  }
+}
+
 const Home=()=> {
   return (
+    <ThemeProvider theme={theme}>
     <div css={styles.div}>
       <Header />
       <Frame />
       <Footer />
     </div>
+    </ThemeProvider>
   );
 }
 
