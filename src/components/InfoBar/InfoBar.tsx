a2d68b4e7d68de22c4f747d8c2d470a7bb800948
Louis Matsika <louis.matsika@bbc.co.uk>
2025-03-02 14:51:08 +0000

    undo api calls for now

--- src/components/InfoBar/InfoBar.tsx
+++ src/components/InfoBar/InfoBar.tsx
@@ -1,29 +1,13 @@
 /** @jsxImportSource @emotion/react */
 import styles from "./index.styles";
 
-async function getData() {
-  const res = await fetch('/api/featuredMusic', {
-    cache: 'force-cache', // This caches the result
-    next: {
-      revalidate: 3600 // Revalidate every hour (optional)
-    }
-  });
-
-  if (!res.ok) {
-    throw new Error('Failed to fetch data');
-  }
-
-  return res.json();
-}
-
 async function InfoBar() {
-  const music = await getData();
 
   return (
     <div css={styles.div}>
       <h1>SAI's fate</h1>
-      <p>{music.title}</p>
-      <p>{music.type}</p>
+      <p>place holder 1</p>
+      <p>place holder 2</p>
     </div>
   );
 }
