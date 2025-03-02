6c3b198b9fb2b13f9ab68aff4ca28f12180da8c7
Maiken Gunnerod <maiken.gunneroed@gmail.com>
2025-03-02 17:14:25 +0100

    add frame mq

--- src/components/Frame/index.styles.ts
+++ src/components/Frame/index.styles.ts
@@ -1,5 +1,5 @@
 import {css} from "@emotion/react";
-import {colours, spacings, breakpoints} from "#/themes/theme";
+import {colours, spacings, breakpoints, pictureWidths} from "#/themes/theme";
 
 export default {
     frameContainer: () =>
@@ -7,14 +7,20 @@ export default {
             display: "flex",
             justifyContent: "center",
             margin: `${spacings.QUINTUPLE}`,
-
-            [`@media (max-width: ${breakpoints.GROUP_3_COMPUTER})`]: {
-                backgroundColor: `${colours.NARDOGREY}`
-            }
         }),
     art: () =>
         css({
             border: `1px solid ${colours.NARDOGREY}`,
             borderRadius: `${spacings.FULL}`,
+
+            [`@media (max-width: ${breakpoints.GROUP_2_TABLET})`]: {
+                width: `${pictureWidths.PICTURE_2_TABLET}`,
+            },
+            [`@media (max-width: ${breakpoints.GROUP_1_SMARTPHONE})`]: {
+                width: `${pictureWidths.PICTURE_1_SMARTPHONE}`,
+            },
+            [`@media (max-width: ${breakpoints.GROUP_0_FLIPPHONE})`]: {
+                width: `${pictureWidths.PICTURE_0_FLIPPHONE}`,
+            },
         }),
 };
