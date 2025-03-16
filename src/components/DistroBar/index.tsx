7b990ccb5c9224c38afe996da42cfc0274d319a5
Louis Matsika <louis.matsika@bbc.co.uk>
2025-03-16 12:29:53 +0000

    add a button

--- src/components/DistroBar/index.tsx
+++ src/components/DistroBar/index.tsx
@@ -1,11 +1,19 @@
 import styles from "./styles.module.scss";
 
+const MusicButton = () => {
+  return (
+    <a href="https://example.com" target="_blank" rel="noopener noreferrer">
+      button
+    </a>
+  );
+};
+
 const DistroBar = () => {
-    return (
-        <div className={styles.distroBar}>
-            distro bar
-        </div>
-    );
+  return (
+    <div className={styles.distroBar}>
+      <MusicButton />
+    </div>
+  );
 };
 
 export default DistroBar;
--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,7 @@ import styles from "./styles.module.scss";
 const Footer = () => {
     return (
         <footer className={styles.footer}>
-            <a className={styles.link} href="https://github.com/Louis-Matsika/fate" target="_blank">
+            <a className={styles.link} href="https://github.com/Louis-Matsika/fate" target="_blank" rel="noopener noreferrer">
                 SAI's fate. repo
             </a>
         </footer>
