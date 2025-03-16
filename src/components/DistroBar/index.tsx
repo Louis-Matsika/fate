eba7ccef9d3e60dd82cae1c16efa78684eb7691d
Louis Matsika <louis.matsika@bbc.co.uk>
2025-03-16 11:56:29 +0000

    start on distro bar

---
+++ src/components/DistroBar/index.tsx
@@ -0,0 +1,11 @@
+import styles from "./styles.module.scss";
+
+const DistroBar = () => {
+    return (
+        <div className={styles.distroBar}>
+            distro bar
+        </div>
+    );
+};
+
+export default DistroBar;
