6502aa2a6926e4f557e10f557fed6b81bf8b7dac
Louis Matsika <louis.matsika@bbc.co.uk>
2025-06-25 17:43:45 +0100

    add meta tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,6 +12,19 @@ export default function RootLayout({
     return (
         <>
         <html lang="en">
+        <head>
+            <title>SAI's fate. — Music, and Creative Projects</title>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="description" content="Discover the music and creative works of SAI. Listen to tracks, explore albums, and connect with SAI’s artistic journey." />
+            <link rel="canonical" href="https://saisfate.com/" />
+            <meta name="robots" content="index, follow" />
+            {/* Open Graph tags */}
+            <meta property="og:title" content="SAI's fate — Music, and Creative Projects" />
+            <meta property="og:description" content="Discover the music and creative works of SAI. Listen to tracks, explore albums, and connect with SAI’s artistic journey." />
+            <meta property="og:image" content="https://i.scdn.co/image/ab6761670000ecd413b162c04f042dedb1000f34" />
+            <meta property="og:url" content="https://saisfate.com/" />
+        </head>
             <body className={styles.appBody}>
                 <Header />
             {children}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Exhibit from "#/components/Exhibit";
 import VideoEmbed from "../components/VideoEmbed/VideoEmbed";
 import DividingLine from "#/themes/DividingLine/DividingLine";
 import AboutBar from "#/components/AboutBar";
+import sai from "#/data/sai.json";
 
 import "./global.css";
 import styles from "./styles.module.scss";
@@ -12,7 +13,7 @@ const Home = () => {
   return (
     <>
       <main className={styles.main}>
-        <Exhibit id="SYSTEMSTHINKING" fullDescription={false} />
+        <Exhibit id={sai.featured} fullDescription={false} />
         <DividingLine />
         <div className={styles.VideoEmbedWrapper}>
           <VideoEmbed
