eb092bec4e9b568d8bc986593ae10ac7deb53aa4
Louis Matsika <54526320+Louis-Matsika@users.noreply.github.com>
2025-09-03 09:09:10 +0100

    Revert "google analytics"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,62 +1,36 @@
 import Header from "#/components/Header/Header";
 import Footer from "#/components/Footer/Footer";
-import Script from "next/script";
 
-import "./global.css";
+import "./global.css"
 import styles from "./styles.module.scss";
 
 export default function RootLayout({
-  children,
+    children,
 }: Readonly<{
-  children: React.ReactNode;
+    children: React.ReactNode;
 }>) {
-  return (
-    <>
-      <html lang="en">
+    return (
+        <>
+        <html lang="en">
         <head>
-          <title>SAI's fate. — Music, and Creative Projects</title>
-          <meta charSet="UTF-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta
-            name="description"
-            content="Discover the music and creative works of SAI. Listen to tracks, explore albums, and connect with SAI’s artistic journey."
-          />
-          <link rel="canonical" href="https://saisfate.com/" />
-          <meta name="robots" content="index, follow" />
-          {/* Open Graph tags */}
-          <meta
-            property="og:title"
-            content="SAI's fate — Music, and Creative Projects"
-          />
-          <meta
-            property="og:description"
-            content="Discover the music and creative works of SAI. Listen to tracks, explore albums, and connect with SAI’s artistic journey."
-          />
-          <meta
-            property="og:image"
-            content="https://i1.sndcdn.com/avatars-w25QY244PCqsouCx-amX68g-t200x200.jpg"
-          />
-          <meta property="og:url" content="https://saisfate.com/" />
+            <title>SAI's fate. — Music, and Creative Projects</title>
+            <meta charSet="UTF-8" />
+            <meta name="viewport" content="width=device-width, initial-scale=1" />
+            <meta name="description" content="Discover the music and creative works of SAI. Listen to tracks, explore albums, and connect with SAI’s artistic journey." />
+            <link rel="canonical" href="https://saisfate.com/" />
+            <meta name="robots" content="index, follow" />
+            {/* Open Graph tags */}
+            <meta property="og:title" content="SAI's fate — Music, and Creative Projects" />
+            <meta property="og:description" content="Discover the music and creative works of SAI. Listen to tracks, explore albums, and connect with SAI’s artistic journey." />
+            <meta property="og:image" content="https://i1.sndcdn.com/avatars-w25QY244PCqsouCx-amX68g-t200x200.jpg" />
+            <meta property="og:url" content="https://saisfate.com/" />
         </head>
-        <body className={styles.appBody}>
-            {/* Google tag (gtag.js) */}
-            <Script
-            src={`https://www.googletagmanager.com/gtag/js?id=G-H34JSN2G39`}
-            strategy="afterInteractive"
-          />
-            <Script id="ga-gtag" strategy="afterInteractive">
-            {`
-                            window.dataLayer = window.dataLayer || [];
-                            function gtag(){dataLayer.push(arguments);}
-                            gtag('js', new Date());
-                            gtag('config', 'G-H34JSN2G39');
-                        `}
-          </Script>
-          <Header />
-          {children}
-          <Footer />
-        </body>
-      </html>
-    </>
-  );
+            <body className={styles.appBody}>
+                <Header />
+            {children}
+                <Footer />
+            </body>
+        </html>
+        </>
+    );
 }
